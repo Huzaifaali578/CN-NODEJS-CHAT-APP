81c4a48a696b0239b43b561bcde46f9c987aba8e
Huzaifa Nagariya <huzaifaali578@gmail.com>
2024-08-26 16:54:39 +0530

    starting server

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-<p> hello world</p>
+import "./env.js"
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+import http from "http";
+
+
+const app = express();
+
+// 1. create server using http.
+const server = http.createServer(app);
+
+// 2. create socket srver.
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// use socket event.
+io.on("connection", (socket) => {
+    console.log("Connection is established");
+
+    socket.on("disconnect", () => {
+        console.log("Connection is Disconnected");
+    });
+});
+
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined 
+server.listen(port, () => {
+    console.log("server is listening on PORT 3000")
+})
